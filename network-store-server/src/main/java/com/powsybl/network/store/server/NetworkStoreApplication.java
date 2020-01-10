ff81c3fbc81880026107e20c0ebad91f1c200948
Jon Harper <jon.harper87@gmail.com>
2020-01-10 13:16:57 +0100

    Use extern parent pom
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

--- network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
+++ network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
@@ -3,6 +3,7 @@ package com.powsybl.network.store.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class NetworkStoreApplication {
 
