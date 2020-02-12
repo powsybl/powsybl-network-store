a9bbe9a6e645d546bcf75265f0a5d1631f200b66
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2020-02-12 11:17:45 +0100

    Addition of missing headers
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- network-store-client/src/main/java/com/powsybl/network/store/client/InternalConnectionImpl.java
+++ network-store-client/src/main/java/com/powsybl/network/store/client/InternalConnectionImpl.java
@@ -1,9 +1,18 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.client;
 
 import com.powsybl.iidm.network.VoltageLevel;
 import com.powsybl.network.store.model.InternalConnectionAttributes;
 import com.powsybl.network.store.model.Resource;
 
+/**
+ * @author Nicolas Noir <nicolas.noir at rte-france.com>
+ */
 public class InternalConnectionImpl implements VoltageLevel.NodeBreakerView.InternalConnection {
 
     private final Resource<InternalConnectionAttributes> resource;
--- network-store-integration-test/src/test/java/com/powsybl/network/store/integration/NetworkImporter.java
+++ network-store-integration-test/src/test/java/com/powsybl/network/store/integration/NetworkImporter.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.integration;
 
 import com.powsybl.iidm.network.Network;
@@ -6,6 +12,9 @@ import com.powsybl.network.store.client.PreloadingStrategy;
 
 import java.nio.file.Paths;
 
+/**
+ * @author Nicolas Noir <nicolas.noir at rte-france.com>
+ */
 public final class NetworkImporter {
     public static void main(String[] args) throws Exception {
         String baseUrl = "http://localhost:8080/";
