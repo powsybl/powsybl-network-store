69633559c4b85bb17f520bbd98570056e9f1c831
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2020-03-30 16:59:38 +0200

    Addition of missing header for NetworkCacheHandler
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- network-store-client/src/main/java/com/powsybl/network/store/client/NetworkCacheHandler.java
+++ network-store-client/src/main/java/com/powsybl/network/store/client/NetworkCacheHandler.java
@@ -1,9 +1,18 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.client;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * @author Nicolas Noir <nicolas.noir at rte-france.com>
+ */
 public class NetworkCacheHandler {
 
     private final Map<UUID, NetworkCache> networkCaches = new HashMap<>();
