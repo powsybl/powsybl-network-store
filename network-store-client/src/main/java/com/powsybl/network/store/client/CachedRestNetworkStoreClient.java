eab215f43fda925a022bc44ea5b3c44c66538b74
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2020-03-30 15:19:08 +0200

    Addition of missing header
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- network-store-client/src/main/java/com/powsybl/network/store/client/CachedRestNetworkStoreClient.java
+++ network-store-client/src/main/java/com/powsybl/network/store/client/CachedRestNetworkStoreClient.java
@@ -1,9 +1,18 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.client;
 
 import com.powsybl.network.store.model.*;
 
 import java.util.*;
 
+/**
+ * @author Nicolas Noir <nicolas.noir at rte-france.com>
+ */
 public class CachedRestNetworkStoreClient implements NetworkStoreClient {
 
     private final BufferedRestNetworkStoreClient bufferedRestNetworkStoreClient;
