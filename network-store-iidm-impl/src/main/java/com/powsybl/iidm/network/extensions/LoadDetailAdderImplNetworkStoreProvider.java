dd7822e9d0abb1473e86a38e87f81298c22d084a
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-08-25 15:06:34 +0200

    Add load detail provider to avoid exception
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ network-store-iidm-impl/src/main/java/com/powsybl/iidm/network/extensions/LoadDetailAdderImplNetworkStoreProvider.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.extensions;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.commons.extensions.ExtensionAdderProvider;
+import com.powsybl.iidm.network.Load;
+
+/**
+ *  @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+@AutoService(ExtensionAdderProvider.class)
+public class LoadDetailAdderImplNetworkStoreProvider implements
+        ExtensionAdderProvider<Load, LoadDetail, LoadDetailAdderImpl> {
+
+    @Override
+    public String getImplementationName() {
+        return "NetworkStore";
+    }
+
+    @Override
+    public Class<LoadDetailAdderImpl> getAdderClass() {
+        return LoadDetailAdderImpl.class;
+    }
+
+    @Override
+    public LoadDetailAdderImpl newAdder(Load load) {
+        return new LoadDetailAdderImpl(load);
+    }
+}
