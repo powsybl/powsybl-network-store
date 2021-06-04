ef72d44879e047bc84a1de278e4c38f5d7b2b894
HOMER Etienne <etiennehomer@gmail.com>
2021-06-04 09:52:15 +0200

    Remove import
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- network-store-model/src/main/java/com/powsybl/network/store/model/CgmesIidmMappingAttributesSerializer.java
+++ network-store-model/src/main/java/com/powsybl/network/store/model/CgmesIidmMappingAttributesSerializer.java
@@ -1,7 +1,6 @@
 package com.powsybl.network.store.model;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
