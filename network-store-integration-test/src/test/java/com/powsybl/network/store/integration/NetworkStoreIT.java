3bfb2b4a2659d1468275de1172351b78a018a028
HOMER Etienne <etiennehomer@gmail.com>
2021-06-14 16:06:53 +0200

    remove unused import
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- network-store-integration-test/src/test/java/com/powsybl/network/store/integration/NetworkStoreIT.java
+++ network-store-integration-test/src/test/java/com/powsybl/network/store/integration/NetworkStoreIT.java
@@ -44,7 +44,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
