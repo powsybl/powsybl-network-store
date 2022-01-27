9085a7d2079c6ec977ac32b6e0f4caf72419c7db
sBouzols <sylvain.bouzols@gmail.com>
2022-01-27 19:05:27 +0100

    use JsonUtil objectMapper in network-store-server
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
+++ network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
@@ -1,7 +1,14 @@
 package com.powsybl.network.store.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.powsybl.commons.json.JsonUtil;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
@@ -9,5 +16,12 @@ public class NetworkStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetworkStoreApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = JsonUtil.createObjectMapper();
+        objectMapper.registerModule(new JodaModule());
+        return objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
