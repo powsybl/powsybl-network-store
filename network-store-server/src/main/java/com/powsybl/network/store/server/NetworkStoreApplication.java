af674e17693fa2facf3ae739cfeabc7f808928ab
Franck LECUYER <franck.lecuyer@rte-france.com>
2022-02-03 13:37:34 +0100

    Remove server side objectmapper customization Add integration test with NaN values
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
+++ network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
@@ -1,14 +1,7 @@
 package com.powsybl.network.store.server;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
@@ -16,12 +9,5 @@ public class NetworkStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetworkStoreApplication.class, args);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
-        objectMapper.registerModule(new JodaModule());
-        return objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
