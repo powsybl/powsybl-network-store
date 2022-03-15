fb33498cf9ca84b55992af0ba294b9e0c23e8b27
HARPER Jon <jon.harper87@gmail.com>
2022-03-15 11:06:45 +0100

    wip

---
+++ network-store-iidm-tck/src/test/java/com/powsybl/network/store/tck/MultiVariantNetworkIT.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.network.store.tck;
+
+import com.powsybl.iidm.network.tck.AbstractMultiVariantNetworkTest;
+import com.powsybl.network.store.server.NetworkStoreApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextHierarchy({
+    @ContextConfiguration(classes = {NetworkStoreApplication.class})
+    })
+@TestPropertySource(properties = { "spring.config.location=classpath:application.yaml" })
+public class MultiVariantNetworkIT extends AbstractMultiVariantNetworkTest {
+
+
+
+}
