c18efd3e4490ac37b0b3cc6481e09c9face3a337
Jon Harper <jon.harper87@gmail.com>
2022-03-28 10:45:28 +0200

    Hacky migration to postgresql (#201)
    Signed-off-by: HARPER Jon <jon.harper87@gmail.com>
    Co-authored-by: Franck LECUYER <franck.lecuyer@rte-france.com>

---
+++ network-store-server/src/main/java/com/powsybl/network/store/server/ResultSet.java
@@ -0,0 +1,290 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.network.store.server;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.UUID;
+
+import java.sql.Clob;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ResultSet implements Iterable<Row>, AutoCloseable {
+
+    java.sql.PreparedStatement preparedStatement;
+    java.sql.ResultSet resultSet;
+
+    public ResultSet(java.sql.PreparedStatement preparedStatement, java.sql.ResultSet resultSet) {
+        this.preparedStatement = preparedStatement;
+        this.resultSet = resultSet;
+    }
+
+    @Override
+    public Iterator<Row> iterator() {
+        return new Iterator<Row>() {
+            boolean moved = false;
+            boolean hasmore = false;
+
+            private void ensureNext() {
+                if (!moved) {
+                    try {
+                        moved = true;
+                        hasmore = resultSet.next();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                ensureNext();
+                return hasmore;
+            }
+
+            @Override
+            public Row next() {
+                ensureNext();
+                if (!hasmore) {
+                    throw new NoSuchElementException();
+                }
+                moved = false;
+                return new Row(ResultSet.this);
+            }
+        };
+    }
+
+    public Row one() {
+        try {
+            boolean b = resultSet.next();
+            if (b) {
+                return new Row(ResultSet.this);
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        Exception exceptionResultSet = null;
+        Exception exceptionPreparedStatement = null;
+        try {
+            resultSet.close();
+        } catch (Exception e) {
+            exceptionResultSet = e;
+        }
+        try {
+            preparedStatement.close();
+        } catch (Exception e) {
+            exceptionPreparedStatement = e;
+        }
+        if (exceptionResultSet != null && exceptionPreparedStatement != null) {
+            RuntimeException r = new RuntimeException(exceptionResultSet);
+            r.addSuppressed(exceptionPreparedStatement);
+            throw r;
+        } else if (exceptionResultSet != null) {
+            throw new RuntimeException(exceptionResultSet);
+        } else if (exceptionPreparedStatement != null) {
+            throw new RuntimeException(exceptionPreparedStatement);
+        }
+    }
+}
+
+class Row {
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    ResultSet resultSet;
+
+    public Row(ResultSet resultSet) {
+        this.resultSet = resultSet;
+    }
+
+    public static boolean isCustomTypeJsonified(Class<?> class1) {
+        return !(
+                  Integer.class.equals(class1) || Long.class.equals(class1)
+                  || Float.class.equals(class1) || Double.class.equals(class1)
+                  || String.class.equals(class1) || Boolean.class.equals(class1)
+                  || UUID.class.equals(class1)
+                  || Date.class.isAssignableFrom(class1) // java.util.Date and java.sql.Date
+            );
+    }
+
+    public <T> T get(int i, Class<T> class1) {
+        try {
+            Object o = resultSet.resultSet.getObject(i + 1);
+            if (o != null && isCustomTypeJsonified(class1)) {
+                if (o instanceof Clob) {
+                    Clob clob = (Clob) o;
+                    o = clob.getSubString(1, (int) clob.length());
+                }
+                try {
+                    return new ObjectMapper().readValue((String) o, class1);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (o instanceof BigDecimal) {
+                return (T) (Object) ((BigDecimal) o).intValue();
+            } else {
+                return (T) o;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean getBoolean(int i) {
+        try {
+            return resultSet.resultSet.getBoolean(i + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public float getFloat(int i) {
+        try {
+            return resultSet.resultSet.getFloat(i + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public double getDouble(int i) {
+        try {
+            return resultSet.resultSet.getDouble(i + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getInt(int i) {
+        try {
+            return resultSet.resultSet.getInt(i + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getString(int i) {
+        try {
+            return resultSet.resultSet.getString(i + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Date getTimestamp(int i) {
+        try {
+            return resultSet.resultSet.getDate(i + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public UUID getUuid(int i) {
+        try {
+            return (UUID) resultSet.resultSet.getObject(i + 1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> getList(int i, Class<T> class1) {
+        try {
+            Object o = resultSet.resultSet.getObject(i + 1);
+            if (o != null) {
+                if (o instanceof Clob) {
+                    Clob clob = (Clob) o;
+                    o = clob.getSubString(1, (int) clob.length());
+                }
+                try {
+                    return (List<T>) mapper.readValue((String) o,
+                            mapper.getTypeFactory().constructCollectionType(List.class, class1));
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T, U> Map<T, U> getMap(int i, Class<T> class1, Class<U> class2) {
+        try {
+            Object o = resultSet.resultSet.getObject(i + 1);
+            if (o != null) {
+                if (o instanceof Clob) {
+                    Clob clob = (Clob) o;
+                    o = clob.getSubString(1,  (int) clob.length());
+                }
+                try {
+                    return (Map<T, U>) mapper.readValue((String) o,
+                            mapper.getTypeFactory().constructMapType(Map.class, class1, class2));
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Set<T> getSet(int i, Class<T> class1) {
+        try {
+            Object o = resultSet.resultSet.getObject(i + 1);
+            if (o != null) {
+                if (o instanceof Clob) {
+                    Clob clob = (Clob) o;
+                    o = clob.getSubString(1, (int) clob.length());
+                }
+                try {
+                    return (Set<T>) mapper.readValue((String) o,
+                            mapper.getTypeFactory().constructCollectionType(Set.class, class1));
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isNull(int i) {
+        try {
+            return resultSet.resultSet.getObject(i + 1) == null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Instant getInstant(int i) {
+        try {
+            return ((java.sql.Timestamp) resultSet.resultSet.getObject(i + 1)).toInstant();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ network-store-server/src/main/java/com/powsybl/network/store/server/Session.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.network.store.server;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.network.store.server.QueryBuilder.BoundStatement;
+import com.powsybl.network.store.server.QueryBuilder.Select;
+import com.powsybl.network.store.server.QueryBuilder.SimpleStatement;
+
+public class Session {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Session.class);
+    Connection conn;
+
+    public Session(Connection conn) {
+        this.conn = conn;
+    }
+
+    public PreparedStatement prepare(SimpleStatement o) {
+        String s = o.getQuery();
+        return new PreparedStatement(s, conn);
+    }
+
+    public ResultSet execute(SimpleStatement o) {
+        try {
+            String s = o.getQuery();
+            java.sql.PreparedStatement ps = conn.prepareStatement(s);
+            int idx = 0;
+            for (Object obj : o.values()) {
+                ps.setObject(++idx, obj);
+            }
+            if (o instanceof Select) {
+                return new ResultSet(ps, ps.executeQuery());
+            } else {
+                ps.executeUpdate();
+                ps.close();
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new PowsyblException(e);
+        }
+    }
+
+    private void cleanExecute(boolean mainThrows, Collection<BoundStatement> statements) throws SQLException {
+        // close all the statements and reset setAutoCommit even if one the first statements throws an exception.
+        // Don't bother throwing if main already threw, instead log directly, because this is called in the finally block.
+        // This avoids the difficulty of not shadowing any throwable coming from the main block.
+        SQLException firstException = null;
+        for (BoundStatement statement : statements) {
+            try {
+                ((PreparedStatement) statement).tlPs.get().close();
+            } catch (SQLException e) {
+                if (firstException != null) {
+                    firstException.addSuppressed(e);
+                } else {
+                    firstException = e;
+                }
+            }
+            ((PreparedStatement) statement).tlPs.remove();
+        }
+        try {
+            conn.setAutoCommit(true);
+        } catch (SQLException e) {
+            if (firstException != null) {
+                firstException.addSuppressed(e);
+            } else {
+                firstException = e;
+            }
+        }
+        if (firstException != null) {
+            if (mainThrows) {
+                LOGGER.error("Additional error closing SQL batch statement", firstException);
+            } else {
+                throw firstException;
+            }
+        }
+    }
+
+    private void doExecute(BatchStatement batch) throws SQLException {
+        boolean mainThrows = true;
+        try {
+            conn.setAutoCommit(false);
+            for (BoundStatement statement : batch.preparedStatements) {
+                ((PreparedStatement) statement).tlPs.get().executeBatch();
+            }
+            mainThrows = false;
+        } finally {
+            cleanExecute(mainThrows, batch.preparedStatements);
+        }
+        for (SimpleStatement statement : batch.statements2) {
+            execute(statement);
+        }
+    }
+
+    public void execute(BatchStatement batch) {
+        try {
+            doExecute(batch);
+        } catch (SQLException e) {
+            throw new PowsyblException(e);
+        }
+    }
+
+}
