76cf4d0a3ff0d826f1f013cc9e764b6025facc44
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-07-04 17:54:22 +0200

    Clean
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/NpeGetConnectableTerminalWhenRemovingLineTest.java
+++ network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/NpeGetConnectableTerminalWhenRemovingLineTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.iidm.impl;
 
 import com.powsybl.iidm.network.Bus;
@@ -10,9 +16,9 @@ import org.junit.Test;
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
-public class NpeGetConnectableTerminalWhenRemovingLine {
+public class NpeGetConnectableTerminalWhenRemovingLineTest {
 
-    @Test
+    @Test(expected = Test.None.class)
     public void test() {
         Network network = FourSubstationsNodeBreakerFactory.create();
         network.getLine("LINE_S2S3").remove();
