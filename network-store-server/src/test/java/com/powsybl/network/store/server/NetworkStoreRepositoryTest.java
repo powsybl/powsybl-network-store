cc9aa9e1c81c0376a839859ddb722be5aeb4e6e1
Sylvain Bouzols <sylvain.bouzols@gmail.com>
2022-09-26 11:49:11 +0200

    Refacto temporary limits owner info (#287)
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- network-store-server/src/test/java/com/powsybl/network/store/server/NetworkStoreRepositoryTest.java
+++ network-store-server/src/test/java/com/powsybl/network/store/server/NetworkStoreRepositoryTest.java
@@ -6,6 +6,7 @@
  */
 package com.powsybl.network.store.server;
 
+import com.powsybl.iidm.network.LimitType;
 import com.powsybl.network.store.model.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,15 +16,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
-import static com.powsybl.network.store.server.Mappings.LINE_TABLE;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class NetworkStoreRepositoryTest {
+
+    private static final UUID NETWORK_UUID = UUID.fromString("7928181c-7977-4592-ba19-88027e4254e4");
 
     @Autowired
     protected NetworkStoreRepository networkStoreRepository;
@@ -33,6 +38,25 @@ public class NetworkStoreRepositoryTest {
 
         String equipmentIdA = "idLineA";
         String equipmentIdB = "idLineB";
+
+        OwnerInfo infoLineA = new OwnerInfo(
+                equipmentIdA,
+                ResourceType.LINE,
+                NETWORK_UUID,
+                Resource.INITIAL_VARIANT_NUM
+        );
+        OwnerInfo infoLineB = new OwnerInfo(
+                equipmentIdB,
+                ResourceType.LINE,
+                NETWORK_UUID,
+                Resource.INITIAL_VARIANT_NUM
+        );
+        OwnerInfo infoLineX = new OwnerInfo(
+                "badID",
+                ResourceType.LINE,
+                NETWORK_UUID,
+                Resource.INITIAL_VARIANT_NUM
+        );
 
         Resource<LineAttributes> resLineA = Resource.lineBuilder()
                 .id(equipmentIdA)
@@ -54,115 +78,104 @@ public class NetworkStoreRepositoryTest {
                         .build())
                 .build();
 
+        assertEquals(resLineA.getId(), infoLineA.getEquipmentId());
+        assertEquals(resLineB.getId(), infoLineB.getEquipmentId());
+        assertNotEquals(resLineA.getId(), infoLineX.getEquipmentId());
+
         TemporaryLimitAttributes templimitAOkSide1a = TemporaryLimitAttributes.builder()
-                .equipmentId(equipmentIdA)
-                .equipmentType(LINE_TABLE)
                 .side(1)
                 .acceptableDuration(100)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
 
         TemporaryLimitAttributes templimitAOkSide2a = TemporaryLimitAttributes.builder()
-                .equipmentId(equipmentIdA)
-                .equipmentType(LINE_TABLE)
                 .side(2)
                 .acceptableDuration(100)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
 
         TemporaryLimitAttributes templimitAOkSide2b = TemporaryLimitAttributes.builder()
-                .equipmentId(equipmentIdA)
-                .equipmentType(LINE_TABLE)
                 .side(2)
                 .acceptableDuration(200)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
 
         // If there are multiple instance of a limit on the same side with the same acceptable duration, only one is kept.
         TemporaryLimitAttributes templimitAOkSide2bSameAcceptableDuration = TemporaryLimitAttributes.builder()
-                .equipmentId(equipmentIdA)
-                .equipmentType(LINE_TABLE)
                 .side(2)
                 .acceptableDuration(200)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
 
         TemporaryLimitAttributes templimitWrongEquipmentId = TemporaryLimitAttributes.builder()
-                .equipmentId("not" + equipmentIdA)
-                .equipmentType(LINE_TABLE)
                 .side(1)
                 .acceptableDuration(100)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
 
         TemporaryLimitAttributes templimitBOkSide1a = TemporaryLimitAttributes.builder()
-                .equipmentId(equipmentIdB)
-                .equipmentType(LINE_TABLE)
                 .side(1)
                 .acceptableDuration(100)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
 
         TemporaryLimitAttributes templimitBOkSide1b = TemporaryLimitAttributes.builder()
-                .equipmentId(equipmentIdB)
-                .equipmentType(LINE_TABLE)
                 .side(1)
                 .acceptableDuration(200)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
 
         TemporaryLimitAttributes templimitBOkSide1c = TemporaryLimitAttributes.builder()
-                .equipmentId(equipmentIdB)
-                .equipmentType(LINE_TABLE)
                 .side(1)
                 .acceptableDuration(300)
-                .limitType(TemporaryLimitType.CURRENT_LIMIT)
+                .limitType(LimitType.CURRENT)
                 .build();
-
-        assertEquals(resLineA.getId(), templimitAOkSide1a.getEquipmentId());
-        assertEquals(resLineA.getId(), templimitAOkSide2a.getEquipmentId());
-        assertEquals(resLineA.getId(), templimitAOkSide2b.getEquipmentId());
-        assertEquals(resLineA.getId(), templimitAOkSide2bSameAcceptableDuration.getEquipmentId());
-        assertNotEquals(resLineA.getId(), templimitWrongEquipmentId.getEquipmentId());
-        assertEquals(resLineB.getId(), templimitBOkSide1a.getEquipmentId());
-        assertEquals(resLineB.getId(), templimitBOkSide1b.getEquipmentId());
-        assertEquals(resLineB.getId(), templimitBOkSide1c.getEquipmentId());
 
         List<Resource<LineAttributes>> lines = new ArrayList<>();
         lines.add(resLineA);
         lines.add(resLineB);
 
-        List<TemporaryLimitAttributes> temporaryLimits = new ArrayList<>();
-        temporaryLimits.add(templimitAOkSide1a);
-        temporaryLimits.add(templimitBOkSide1a);
-        temporaryLimits.add(templimitAOkSide2a);
-        temporaryLimits.add(templimitAOkSide2b);
-        temporaryLimits.add(templimitAOkSide2bSameAcceptableDuration);
-        temporaryLimits.add(templimitBOkSide1b);
-        temporaryLimits.add(templimitWrongEquipmentId);
-        temporaryLimits.add(templimitBOkSide1c);
+        List<TemporaryLimitAttributes> temporaryLimitsA = new ArrayList<>();
+        temporaryLimitsA.add(templimitAOkSide1a);
+        temporaryLimitsA.add(templimitAOkSide2a);
+        temporaryLimitsA.add(templimitAOkSide2b);
+        temporaryLimitsA.add(templimitAOkSide2bSameAcceptableDuration);
+
+        List<TemporaryLimitAttributes> temporaryLimitsB = new ArrayList<>();
+        temporaryLimitsB.add(templimitBOkSide1a);
+        temporaryLimitsB.add(templimitBOkSide1b);
+        temporaryLimitsB.add(templimitBOkSide1c);
+
+        List<TemporaryLimitAttributes> temporaryLimitsX = new ArrayList<>();
+        temporaryLimitsX.add(templimitWrongEquipmentId);
+
+        Map<OwnerInfo, List<TemporaryLimitAttributes>> map = new HashMap<>();
+
+        map.put(infoLineA, temporaryLimitsA);
+        map.put(infoLineB, temporaryLimitsB);
+        map.put(infoLineX, temporaryLimitsX);
 
         assertNull(resLineA.getAttributes().getCurrentLimits1().getTemporaryLimits());
         assertNull(resLineA.getAttributes().getCurrentLimits2());
         assertNull(resLineB.getAttributes().getCurrentLimits1().getTemporaryLimits());
         assertNull(resLineB.getAttributes().getCurrentLimits2());
 
-        networkStoreRepository.insertTemporaryLimitsInEquipments(lines, new ArrayList<>());
+        networkStoreRepository.insertTemporaryLimitsInEquipments(NETWORK_UUID, lines, new HashMap<>());
 
         assertNull(resLineA.getAttributes().getCurrentLimits1().getTemporaryLimits());
         assertNull(resLineA.getAttributes().getCurrentLimits2());
         assertNull(resLineB.getAttributes().getCurrentLimits1().getTemporaryLimits());
         assertNull(resLineB.getAttributes().getCurrentLimits2());
 
-        networkStoreRepository.insertTemporaryLimitsInEquipments(lines, temporaryLimits);
-
+        networkStoreRepository.insertTemporaryLimitsInEquipments(NETWORK_UUID, lines, map);
         assertNotNull(resLineA.getAttributes().getCurrentLimits1().getTemporaryLimits());
         assertNotNull(resLineA.getAttributes().getCurrentLimits2().getTemporaryLimits());
-        assertNotNull(resLineB.getAttributes().getCurrentLimits1().getTemporaryLimits());
-        assertNull(resLineB.getAttributes().getCurrentLimits2());
         assertEquals(1, resLineA.getAttributes().getCurrentLimits1().getTemporaryLimits().size());
         assertEquals(2, resLineA.getAttributes().getCurrentLimits2().getTemporaryLimits().size());
+        assertNotNull(resLineB.getAttributes().getCurrentLimits1().getTemporaryLimits());
+        assertNull(resLineB.getAttributes().getCurrentLimits2());
         assertEquals(3, resLineB.getAttributes().getCurrentLimits1().getTemporaryLimits().size());
+
     }
 
 }
