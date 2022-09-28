6f45ca92e0cd1374294a58ce17f0893071cb69db
Etienne Homer <etiennehomer@gmail.com>
2022-09-28 17:12:00 +0200

    add header and license
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/extensions/IdentifiableShortCircuitAdderImpl.java
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/extensions/IdentifiableShortCircuitAdderImpl.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.iidm.impl.extensions;
 
 import com.powsybl.commons.extensions.AbstractExtensionAdder;
@@ -5,6 +11,9 @@ import com.powsybl.iidm.network.Identifiable;
 import com.powsybl.iidm.network.extensions.IdentifiableShortCircuit;
 import com.powsybl.iidm.network.extensions.IdentifiableShortCircuitAdder;
 
+/**
+ * @author Etienne Homer <etienne.homer at rte-france.com>
+ */
 public class IdentifiableShortCircuitAdderImpl<I extends Identifiable<I>> extends AbstractExtensionAdder<I, IdentifiableShortCircuit<I>>
         implements IdentifiableShortCircuitAdder<I> {
 
