bdfc0b9eb7e9e20f40f66c8776fd699c3e127995
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-10-07 08:32:12 +0200

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
+++ network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
@@ -3,8 +3,15 @@ package com.powsybl.network.store.server;
 import com.powsybl.ws.commons.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import liquibase.configuration.LiquibaseConfiguration;
+import liquibase.logging.core.LogServiceFactory;
+import liquibase.license.LicenseServiceFactory;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.nativex.hint.TypeHint;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@TypeHint(types = {SimpleDriverDataSource.class, LiquibaseConfiguration.class, LogServiceFactory.class,
+        LicenseServiceFactory.class})
 @SpringBootApplication
 public class NetworkStoreApplication {
 
