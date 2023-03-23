e82604f40727d699a60abdab59be4f754d3a11ba
Slimane AMAR <slimane.amar@rte-france.com>
2023-03-23 17:47:49 +0100

    When creating 'BusbarSectionPosition' extension the positions must be valid and are now checked
    Signed-off-by: Slimane AMAR <slimane.amar@rte-france.com>

--- network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/VoltageLevelTest.java
+++ network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/VoltageLevelTest.java
@@ -7,10 +7,11 @@
 package com.powsybl.network.store.iidm.impl;
 
 import com.powsybl.iidm.network.*;
+import com.powsybl.iidm.network.extensions.BusbarSectionPositionAdder;
 import com.powsybl.iidm.network.tck.AbstractVoltageLevelTest;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * @author Slimane Amar <slimane.amar at rte-france.com>
@@ -36,6 +37,53 @@ public class VoltageLevelTest extends AbstractVoltageLevelTest {
         assertEquals(network.getGenerator("G"), network.getVoltageLevel("VL1").getConnectable("G", Generator.class));
         assertEquals(network.getLoad("L"), network.getVoltageLevel("VL1").getConnectable("L", Load.class));
         assertEquals(network.getBusbarSection("BBS1"), network.getVoltageLevel("VL1").getConnectable("BBS1", BusbarSection.class));
+    }
+
+    @Test
+    public void testBusbarSectionPositions() {
+        Network network = Network.create("test", "test");
+        Substation s = network.newSubstation()
+            .setId("S")
+            .setCountry(Country.FR)
+            .add();
+        VoltageLevel vl = s.newVoltageLevel()
+            .setId("VL")
+            .setNominalV(400.0)
+            .setTopologyKind(TopologyKind.NODE_BREAKER)
+            .add();
+
+        BusbarSection bbs = vl.getNodeBreakerView().newBusbarSection()
+            .setId("idBBS")
+            .setName("nameBBS")
+            .setNode(0)
+            .add();
+        bbs.newExtension(BusbarSectionPositionAdder.class).withBusbarIndex(0).withSectionIndex(0).add();
+        assertNotNull(network.getVoltageLevel("VL"));
+        assertNotNull(network.getVoltageLevel("VL").getNodeBreakerView().getBusbarSection("idBBS"));
+
+        BusbarSectionPositionAdder busbarSectionPositionAdder = bbs.newExtension(BusbarSectionPositionAdder.class);
+        assertEquals("Busbar index has to be greater or equals to zero",
+            assertThrows(IllegalArgumentException.class, busbarSectionPositionAdder::add).getMessage());
+
+        busbarSectionPositionAdder = bbs.newExtension(BusbarSectionPositionAdder.class).withBusbarIndex(0);
+        assertEquals("Busbar index has to be greater or equals to zero",
+            assertThrows(IllegalArgumentException.class, busbarSectionPositionAdder::add).getMessage());
+
+        busbarSectionPositionAdder = bbs.newExtension(BusbarSectionPositionAdder.class).withSectionIndex(0);
+        assertEquals("Busbar index has to be greater or equals to zero",
+            assertThrows(IllegalArgumentException.class, busbarSectionPositionAdder::add).getMessage());
+
+        busbarSectionPositionAdder = bbs.newExtension(BusbarSectionPositionAdder.class).withBusbarIndex(-1).withSectionIndex(0);
+        assertEquals("Busbar index has to be greater or equals to zero",
+            assertThrows(IllegalArgumentException.class, busbarSectionPositionAdder::add).getMessage());
+
+        busbarSectionPositionAdder = bbs.newExtension(BusbarSectionPositionAdder.class).withBusbarIndex(0).withSectionIndex(-1);
+        assertEquals("Busbar index has to be greater or equals to zero",
+            assertThrows(IllegalArgumentException.class, busbarSectionPositionAdder::add).getMessage());
+
+        busbarSectionPositionAdder = bbs.newExtension(BusbarSectionPositionAdder.class).withBusbarIndex(-1).withSectionIndex(-1);
+        assertEquals("Busbar index has to be greater or equals to zero",
+            assertThrows(IllegalArgumentException.class, busbarSectionPositionAdder::add).getMessage());
     }
 
     @Override
