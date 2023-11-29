8c501f479af41097277e7e2fcc31edd31007631e
Nicolas Rol <nicolas.rol@rte-france.com>
2023-11-29 10:43:26 +0100

    bump to powsybl-core 6.1.1 and powsybl-dependencies 2023.4.0 + add missing author and licence
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/SwitchPathValidator.java
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/SwitchPathValidator.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.network.store.iidm.impl;
 
 import com.powsybl.iidm.network.Switch;
@@ -8,6 +15,9 @@ import org.jgrapht.alg.shortestpath.PathValidator;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+/**
+ * @author Nicolas Rol {@literal <nicolas.rol at rte-france.com>}
+ */
 public class SwitchPathValidator implements PathValidator<Integer, Edge> {
 
     private final Predicate<Switch> isSwitchValidInGraph;
