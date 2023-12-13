410b4b891f48ef77ed67fb9fa9af05c5c12969a3
Florent MILLOT <millotflo@gmail.com>
2023-12-13 09:44:52 +0100

    Add Javadoc
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/PhaseTapChangerStepsReplacerImpl.java
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/PhaseTapChangerStepsReplacerImpl.java
@@ -1,9 +1,19 @@
+/**
+ * Copyright (c) 2023, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.powsybl.network.store.iidm.impl;
 
 import com.powsybl.iidm.network.PhaseTapChangerStepsReplacer;
 import com.powsybl.iidm.network.PhaseTapChangerStepsReplacerStepAdder;
 import com.powsybl.network.store.model.TapChangerStepAttributes;
 
+/**
+ * @author Florent MILLOT {@literal <florent.millot at rte-france.com>}
+ */
 public class PhaseTapChangerStepsReplacerImpl extends AbstractTapChangerStepsReplacer<PhaseTapChangerStepsReplacerImpl> implements PhaseTapChangerStepsReplacer {
 
     class StepAdderImpl implements PhaseTapChangerStepsReplacerStepAdder {
