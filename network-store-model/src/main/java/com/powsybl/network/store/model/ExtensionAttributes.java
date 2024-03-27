ba934a11536248193e8478d5473d54fca2a58764
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2024-03-27 17:54:27 +0100

    Add tests
    Signed-off-by: BOUHOURS Antoine <antoine.bouhours@rte-france.com>

--- network-store-model/src/main/java/com/powsybl/network/store/model/ExtensionAttributes.java
+++ network-store-model/src/main/java/com/powsybl/network/store/model/ExtensionAttributes.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
 
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
         property = "@type"
 )
 @JsonTypeIdResolver(ExtensionAttributesIdResolver.class)
