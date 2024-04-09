970369b2ddce336b9af2d5b137b58f29310bebfa
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2024-04-09 15:40:24 +0200

    Add missing license header and author
    Signed-off-by: BOUHOURS Antoine <antoine.bouhours@rte-france.com>

--- network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/AbstractIdentifiableToInjectionAttributesAdapter.java
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/AbstractIdentifiableToInjectionAttributesAdapter.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.iidm.impl;
 
 import com.powsybl.network.store.model.ExtensionAttributes;
@@ -8,6 +14,9 @@ import com.powsybl.network.store.model.Resource;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @author Antoine Bouhours <antoine.bouhours at rte-france.com>
+ */
 public abstract class AbstractIdentifiableToInjectionAttributesAdapter<T extends IdentifiableAttributes> implements InjectionAttributes {
     protected final T attributes;
 
