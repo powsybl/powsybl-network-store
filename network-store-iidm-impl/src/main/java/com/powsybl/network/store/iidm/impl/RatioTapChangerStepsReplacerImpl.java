f7ff173302cd17d868cef8339778621f3453906a
Florent MILLOT <millotflo@gmail.com>
2024-04-09 11:46:35 +0200

    PR remarks
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/RatioTapChangerStepsReplacerImpl.java
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/RatioTapChangerStepsReplacerImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * Copyright (c) 2024, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -59,7 +59,7 @@ public class RatioTapChangerStepsReplacerImpl extends AbstractTapChangerStepsRep
 
         @Override
         public RatioTapChangerStepsReplacer endStep() {
-            TapChangerStepAttributes phaseTapChangerStepAttributes =
+            TapChangerStepAttributes ratioTapChangerStepAttributes =
                 TapChangerStepAttributes.builder()
                     .b(b)
                     .g(g)
@@ -67,7 +67,7 @@ public class RatioTapChangerStepsReplacerImpl extends AbstractTapChangerStepsRep
                     .rho(rho)
                     .x(x)
                     .build();
-            steps.add(phaseTapChangerStepAttributes);
+            steps.add(ratioTapChangerStepAttributes);
             return RatioTapChangerStepsReplacerImpl.this;
         }
 
