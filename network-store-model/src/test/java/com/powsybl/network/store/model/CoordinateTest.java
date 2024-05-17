299262f09ddeb57e08db8f2d4e569b5506d57912
Seddik Yengui <seddik.yengui@rte-france.com>
2024-05-17 14:39:24 +0200

    add license
    Signed-off-by: Seddik Yengui <seddik.yengui@rte-france.com>

--- network-store-model/src/test/java/com/powsybl/network/store/model/CoordinateTest.java
+++ network-store-model/src/test/java/com/powsybl/network/store/model/CoordinateTest.java
@@ -1,9 +1,20 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.powsybl.network.store.model;
 
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+
+/**
+ * @author Seddik Yengui <seddik.yengui_externe at rte-france.com>
+ */
 
 public class CoordinateTest {
     @Test
