e482bb575469f93b9fdd171d3e8502e40d115631
Seddik Yengui <seddik.yengui@rte-france.com>
2024-05-17 14:36:43 +0200

    add more unit tests
    Signed-off-by: Seddik Yengui <seddik.yengui@rte-france.com>

--- network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/tck/extensions/SubstationAndLinePositionTest.java
+++ network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/tck/extensions/SubstationAndLinePositionTest.java
@@ -6,10 +6,45 @@
  */
 package com.powsybl.network.store.iidm.impl.tck.extensions;
 
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.iidm.network.Line;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.Substation;
+import com.powsybl.iidm.network.VoltageLevel;
+import com.powsybl.iidm.network.extensions.*;
 import com.powsybl.iidm.network.tck.extensions.AbstractSubstationAndLinePositionTest;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 public class SubstationAndLinePositionTest extends AbstractSubstationAndLinePositionTest {
+    @Test
+    public void testLinePositionException() {
+        Network network = EurostagTutorialExample1Factory.create();
+        VoltageLevel vl = network.getVoltageLevel("VLHV1");
+        assertEquals("Line position extension only supported for lines and dangling lines", assertThrows(PowsyblException.class, () -> vl.newExtension(LinePositionAdder.class)).getMessage());
+
+    }
+
+    @Test
+    public void testExtensionsName() {
+        Network network = EurostagTutorialExample1Factory.create();
+
+        Substation p1 = network.getSubstation("P1");
+        SubstationPosition substationPosition = p1.newExtension(SubstationPositionAdder.class).withCoordinate(new Coordinate(48.0D, 2.0D)).add();
+        Assertions.assertEquals("substationPosition", substationPosition.getName());
+
+        Line l1 = network.getLine("NHV1_NHV2_1");
+        l1.newExtension(LinePositionAdder.class).withCoordinates(List.of(new Coordinate(48.0D, 2.0D), new Coordinate(48.1D, 2.1D))).add();
+        LinePosition linePosition = l1.getExtension(LinePosition.class);
+        Assertions.assertEquals("linePosition", linePosition.getName());
+    }
 }
