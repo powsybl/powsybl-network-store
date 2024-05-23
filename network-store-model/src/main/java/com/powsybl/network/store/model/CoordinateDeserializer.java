26e817f4f03d728a4c92d7a6e680eeb836d859ec
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2024-05-23 13:52:34 +0200

    Add missing headers
    Signed-off-by: BOUHOURS Antoine <antoine.bouhours@rte-france.com>

--- network-store-model/src/main/java/com/powsybl/network/store/model/CoordinateDeserializer.java
+++ network-store-model/src/main/java/com/powsybl/network/store/model/CoordinateDeserializer.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.model;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -8,6 +14,9 @@ import com.powsybl.iidm.network.extensions.Coordinate;
 
 import java.io.IOException;
 
+/**
+ * @author Antoine Bouhours <antoine.bouhours at rte-france.com>
+ */
 public class CoordinateDeserializer extends StdDeserializer<Coordinate> {
 
     protected CoordinateDeserializer() {
--- network-store-model/src/test/java/com/powsybl/network/store/model/CoordinateDeserializerTest.java
+++ network-store-model/src/test/java/com/powsybl/network/store/model/CoordinateDeserializerTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.store.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -9,6 +15,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
+/**
+ * @author Antoine Bouhours <antoine.bouhours at rte-france.com>
+ */
 public class CoordinateDeserializerTest {
 
     @Test
