137b7e666050cfd9bbc4b3d96b0aae78ff02969a
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2024-05-27 17:21:42 +0200

    Implement TwoWindingsTransformerToBeEstimated and ThreeWindingsTransformerToBeEstimated extensions. (#397)
    Implement TwoWindingsTransformerToBeEstimated and ThreeWindingsTransformerToBeEstimated extensions.
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

---
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/extensions/ThreeWindingsTransformerToBeEstimatedAdderImpl.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.network.store.iidm.impl.extensions;
+
+import com.powsybl.commons.extensions.AbstractExtensionAdder;
+import com.powsybl.iidm.network.ThreeWindingsTransformer;
+import com.powsybl.iidm.network.extensions.ThreeWindingsTransformerToBeEstimated;
+import com.powsybl.iidm.network.extensions.ThreeWindingsTransformerToBeEstimatedAdder;
+import com.powsybl.network.store.iidm.impl.ThreeWindingsTransformerImpl;
+import com.powsybl.network.store.model.ThreeWindingsTransformerToBeEstimatedAttributes;
+
+/**
+ * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ */
+public class ThreeWindingsTransformerToBeEstimatedAdderImpl extends AbstractExtensionAdder<ThreeWindingsTransformer, ThreeWindingsTransformerToBeEstimated>
+        implements ThreeWindingsTransformerToBeEstimatedAdder {
+
+    private boolean rtc1Status = false;
+    private boolean rtc2Status = false;
+    private boolean rtc3Status = false;
+
+    private boolean ptc1Status = false;
+    private boolean ptc2Status = false;
+    private boolean ptc3Status = false;
+
+    public ThreeWindingsTransformerToBeEstimatedAdderImpl(ThreeWindingsTransformer extendable) {
+        super(extendable);
+    }
+
+    @Override
+    protected ThreeWindingsTransformerToBeEstimated createExtension(ThreeWindingsTransformer threeWindingsTransformer) {
+        ThreeWindingsTransformerToBeEstimatedAttributes attributes = ThreeWindingsTransformerToBeEstimatedAttributes.builder()
+            .rtc1Status(rtc1Status)
+            .rtc2Status(rtc2Status)
+            .rtc3Status(rtc3Status)
+            .ptc1Status(ptc1Status)
+            .ptc2Status(ptc2Status)
+            .ptc3Status(ptc3Status)
+            .build();
+        ((ThreeWindingsTransformerImpl) threeWindingsTransformer).updateResource(res -> res.getAttributes().getExtensionAttributes().put(ThreeWindingsTransformerToBeEstimated.NAME, attributes));
+        return new ThreeWindingsTransformerToBeEstimatedImpl(threeWindingsTransformer);
+    }
+
+    @Override
+    public ThreeWindingsTransformerToBeEstimatedAdder withRatioTapChanger1Status(boolean toBeEstimated) {
+        this.rtc1Status = toBeEstimated;
+        return this;
+    }
+
+    @Override
+    public ThreeWindingsTransformerToBeEstimatedAdder withRatioTapChanger2Status(boolean toBeEstimated) {
+        this.rtc2Status = toBeEstimated;
+        return this;
+    }
+
+    @Override
+    public ThreeWindingsTransformerToBeEstimatedAdder withRatioTapChanger3Status(boolean toBeEstimated) {
+        this.rtc3Status = toBeEstimated;
+        return this;
+    }
+
+    @Override
+    public ThreeWindingsTransformerToBeEstimatedAdder withPhaseTapChanger1Status(boolean toBeEstimated) {
+        this.ptc1Status = toBeEstimated;
+        return this;
+    }
+
+    @Override
+    public ThreeWindingsTransformerToBeEstimatedAdder withPhaseTapChanger2Status(boolean toBeEstimated) {
+        this.ptc2Status = toBeEstimated;
+        return this;
+    }
+
+    @Override
+    public ThreeWindingsTransformerToBeEstimatedAdder withPhaseTapChanger3Status(boolean toBeEstimated) {
+        this.ptc3Status = toBeEstimated;
+        return this;
+    }
+}
