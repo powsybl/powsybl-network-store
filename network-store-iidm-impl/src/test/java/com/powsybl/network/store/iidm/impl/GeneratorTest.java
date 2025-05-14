512d26212d6778c5d0ae1f842db656fbc2ca4442
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2025-05-14 17:46:39 +0200

    Fix id before removal when switching variant
    Signed-off-by: BOUHOURS Antoine <antoine.bouhours@rte-france.com>

--- network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/GeneratorTest.java
+++ network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/GeneratorTest.java
@@ -6,12 +6,11 @@
  */
 package com.powsybl.network.store.iidm.impl;
 
-import com.powsybl.iidm.network.Generator;
-import com.powsybl.iidm.network.Load;
-import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.test.FourSubstationsNodeBreakerFactory;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Etienne Lesot <etienne.lesot at rte-france.com>
@@ -25,21 +24,21 @@ class GeneratorTest {
         // initialization
         Generator generator = network.getGenerator("GH3");
         Load load = network.getLoad("LD1");
-        Assertions.assertTrue(generator.isVoltageRegulatorOn());
-        Assertions.assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
-        Assertions.assertEquals(400, generator.getTargetV());
+        assertTrue(generator.isVoltageRegulatorOn());
+        assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
+        assertEquals(400, generator.getTargetV());
 
         // set the generator's regulation on the load terminal (both equipments are not on the same voltage level/bus)
         generator.setRegulatingTerminal(load.getTerminal());
         generator.setTargetV(225);
-        Assertions.assertTrue(generator.isVoltageRegulatorOn());
-        Assertions.assertEquals(load.getTerminal(), generator.getRegulatingTerminal());
-        Assertions.assertEquals(225, generator.getTargetV());
+        assertTrue(generator.isVoltageRegulatorOn());
+        assertEquals(load.getTerminal(), generator.getRegulatingTerminal());
+        assertEquals(225, generator.getTargetV());
 
         // remove the load
         network.getLoad("LD1").remove();
-        Assertions.assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
-        Assertions.assertFalse(generator.isVoltageRegulatorOn());
+        assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
+        assertFalse(generator.isVoltageRegulatorOn());
     }
 
     @Test
@@ -49,17 +48,32 @@ class GeneratorTest {
         // initialization
         Generator generator = network.getGenerator("GH3");
         Load load = network.getLoad("LD2");
-        Assertions.assertTrue(generator.isVoltageRegulatorOn());
-        Assertions.assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
+        assertTrue(generator.isVoltageRegulatorOn());
+        assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
 
         // set the generator's regulation on the load terminal (both equipments are on the same voltage level/bus)
         generator.setRegulatingTerminal(load.getTerminal());
-        Assertions.assertTrue(generator.isVoltageRegulatorOn());
-        Assertions.assertEquals(load.getTerminal(), generator.getRegulatingTerminal());
+        assertTrue(generator.isVoltageRegulatorOn());
+        assertEquals(load.getTerminal(), generator.getRegulatingTerminal());
 
         // remove the load
         network.getLoad("LD2").remove();
-        Assertions.assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
-        Assertions.assertTrue(generator.isVoltageRegulatorOn());
+        assertEquals(generator.getTerminal(), generator.getRegulatingTerminal());
+        assertTrue(generator.isVoltageRegulatorOn());
+    }
+
+    @Test
+    void testIdBeforeRemovalMultipleVariants() {
+        Network network = FourSubstationsNodeBreakerFactory.create();
+
+        Generator generator = network.getGenerator("GH3");
+        assertNotNull(generator);
+        generator.remove();
+        assertEquals("GH3", generator.getId());
+
+        VariantManager variantManager = network.getVariantManager();
+        variantManager.cloneVariant(VariantManagerConstants.INITIAL_VARIANT_ID, "variant1");
+        variantManager.setWorkingVariant("variant1");
+        assertEquals("GH3", generator.getId());
     }
 }
