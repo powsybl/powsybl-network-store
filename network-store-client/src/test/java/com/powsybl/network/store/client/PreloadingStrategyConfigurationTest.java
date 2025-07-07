68ed7377069262dc5e263a504ac9a134848f8b3a
Joris Mancini <joris.mancini_externe@rte-france.com>
2025-07-07 15:05:33 +0200

    add tests
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

---
+++ network-store-client/src/test/java/com/powsybl/network/store/client/PreloadingStrategyConfigurationTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.network.store.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Joris Mancini <joris.mancini_externe at rte-france.com>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = { PreloadingStrategyConfiguration.class })
+public class PreloadingStrategyConfigurationTest {
+
+    @Autowired
+    private PreloadingStrategyConfiguration preloadingStrategyConfiguration;
+
+    @Test
+    public void testPreloadingStrategyConfiguration() {
+        assertNull(preloadingStrategyConfiguration.preloadingStrategy());
+    }
+}
