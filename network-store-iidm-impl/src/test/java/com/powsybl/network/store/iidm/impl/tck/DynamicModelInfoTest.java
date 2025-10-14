7933edcd1514b1c8c1b53bb1686d1b90e1b681bb
Slimane AMAR <slimane.amar_externe@rte-france.com>
2025-10-14 17:33:43 +0200

    Migration powsybl dependencies v2025.2.0

---
+++ network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/tck/DynamicModelInfoTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.network.store.iidm.impl.tck;
+
+import com.powsybl.iidm.network.tck.extensions.AbstractDynamicModelInfoTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Slimane Amar <slimane.amar at rte-france.com>
+ */
+class DynamicModelInfoTest extends AbstractDynamicModelInfoTest {
+
+    // TODO All tests are ignored (extension is not yet implemented)
+
+    @Test
+    public void variantsCloneTest() {
+
+    }
+
+    @Test
+    public void testExtension() {
+
+    }
+}
