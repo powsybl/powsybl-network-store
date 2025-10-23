4a4cca83bd3dde0e2845ae66d49a8a211a38a90a
Franck LECUYER <franck.lecuyer@rte-france.com>
2025-10-23 16:20:30 +0200

    Upgrade to powsybl-dependencies 2025.2.0 Add implementation of DynamicModelInfo extension (necessary for dynamic simulation) Fix pb creating RemoteReactivePowerControl extension (check on targetQ is done only if enabled boolean is true)
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

---
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/extensions/DynamicModelInfoImpl.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.network.store.iidm.impl.extensions;
+
+import com.powsybl.commons.extensions.AbstractExtension;
+import com.powsybl.iidm.network.Identifiable;
+import com.powsybl.iidm.network.extensions.DynamicModelInfo;
+import com.powsybl.network.store.iidm.impl.AbstractIdentifiableImpl;
+import com.powsybl.network.store.model.DynamicModelInfoAttributes;
+import com.powsybl.network.store.model.Resource;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author Franck Lecuyer {@literal <franck.lecuyer at rte-france.com>}
+ */
+
+// Minimal implementation of DynamicModelInfo extension, necessary to
+// run dynamic simulation analysis and dynamic security analysis in powsybl-dynamo version 3.0.0
+public class DynamicModelInfoImpl<I extends Identifiable<I>> extends AbstractExtension<I> implements DynamicModelInfo<I> {
+
+    public DynamicModelInfoImpl(I extendable) {
+        super(extendable);
+    }
+
+    private AbstractIdentifiableImpl<?, ?> getIdentifiable() {
+        return (AbstractIdentifiableImpl<?, ?>) getExtendable();
+    }
+
+    private DynamicModelInfoAttributes getDynamicModelInfoAttributes() {
+        return (DynamicModelInfoAttributes) getIdentifiable().getResource().getAttributes().getExtensionAttributes().get(DynamicModelInfo.NAME);
+    }
+
+    private static DynamicModelInfoAttributes getAttributes(Resource<?> resource) {
+        return (DynamicModelInfoAttributes) resource.getAttributes();
+    }
+
+    @Override
+    public String getModelName() {
+        return getDynamicModelInfoAttributes().getModelName();
+    }
+
+    @Override
+    public void setModelName(String modelName) {
+        getDynamicModelInfoAttributes().setModelName(modelName);
+        String oldValue = getModelName();
+        if (!StringUtils.equals(oldValue, modelName)) {
+            getIdentifiable().updateResourceExtension(this, res -> getAttributes(res).setModelName(modelName), "modelName", oldValue, modelName);
+        }
+    }
+}
