ed05bc711e0e96d66cc211ffc49f008c021c6c88
Etienne LESOT <etienne.lesot@rte-france.com>
2026-01-06 13:32:36 +0100

    fix checkstyle
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- network-store-client/src/main/java/com/powsybl/network/store/client/CollectionBuffer.java
+++ network-store-client/src/main/java/com/powsybl/network/store/client/CollectionBuffer.java
@@ -87,7 +87,7 @@ public class CollectionBuffer<T extends IdentifiableAttributes> {
                 // there are only 3 filters now:
                 // SV included in BASIC included in WITH_LIMITS
                 // to be updated when some filters like WITH_EXTENSIONS are added
-                if (resourceAndFilter.getAttributeFilter() != null &&  attributeFilter != null) {
+                if (resourceAndFilter.getAttributeFilter() != null && attributeFilter != null) {
                     if (resourceAndFilter.getAttributeFilter().getPriority() > attributeFilter.getPriority()) {
                         resourceAndFilter.setAttributeFilter(resourceAndFilter.getAttributeFilter());
                     } else {
