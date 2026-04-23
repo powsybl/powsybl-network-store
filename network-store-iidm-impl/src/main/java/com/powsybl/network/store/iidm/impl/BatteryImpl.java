d47bfc666ec69350cdca48aa6594d17825275a4e
David BRAQUART <david.braquart@rte-france.com>
2026-04-23 18:02:55 +0200

    fix update battery
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/BatteryImpl.java
+++ network-store-iidm-impl/src/main/java/com/powsybl/network/store/iidm/impl/BatteryImpl.java
@@ -41,7 +41,7 @@ public class BatteryImpl extends AbstractInjectionImpl<Battery, BatteryAttribute
 
     @Override
     public Battery setTargetP(double targetP) {
-        ValidationUtil.checkP0(this, targetP, ValidationLevel.STEADY_STATE_HYPOTHESIS, getNetwork().getReportNodeContext().getReportNode());
+        ValidationUtil.checkP0(this, targetP, getNetwork().getMinValidationLevel(), getNetwork().getReportNodeContext().getReportNode());
         ValidationUtil.checkActivePowerLimits(this, getMinP(), getMaxP());
         double oldValue = getTargetP();
         if (targetP != oldValue) {
@@ -58,7 +58,7 @@ public class BatteryImpl extends AbstractInjectionImpl<Battery, BatteryAttribute
 
     @Override
     public Battery setTargetQ(double targetQ) {
-        ValidationUtil.checkQ0(this, targetQ, ValidationLevel.STEADY_STATE_HYPOTHESIS, getNetwork().getReportNodeContext().getReportNode());
+        ValidationUtil.checkQ0(this, targetQ, getNetwork().getMinValidationLevel(), getNetwork().getReportNodeContext().getReportNode());
         double oldValue = getResource().getAttributes().getTargetQ();
         if (targetQ != oldValue) {
             updateResource(res -> res.getAttributes().setTargetQ(targetQ),
