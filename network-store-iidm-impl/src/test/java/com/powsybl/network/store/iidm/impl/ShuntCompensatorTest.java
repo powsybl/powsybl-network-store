db6de85113b6ed432e6652711418fa3d9420522d
David BRAQUART <david.braquart@rte-france.com>
2026-04-23 18:56:44 +0200

    fix update mcs
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/ShuntCompensatorTest.java
+++ network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/ShuntCompensatorTest.java
@@ -8,11 +8,15 @@ package com.powsybl.network.store.iidm.impl;
 
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.ShuntCompensator;
+import com.powsybl.iidm.network.ValidationException;
+import com.powsybl.iidm.network.ValidationLevel;
 import com.powsybl.iidm.network.extensions.ConnectablePosition;
 import com.powsybl.iidm.network.extensions.ConnectablePositionAdder;
 import com.powsybl.iidm.network.test.FourSubstationsNodeBreakerFactory;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -29,4 +33,29 @@ class ShuntCompensatorTest {
         assertFalse(shuntCompensator.removeExtension(ConnectablePosition.class));
     }
 
+    @Test
+    void updateWithInvalidTargetV() {
+        Network network = FourSubstationsNodeBreakerFactory.create();
+        ShuntCompensator shuntCompensator = network.getShuntCompensator("SHUNT");
+        shuntCompensator.setTargetV(100.0);
+        shuntCompensator.setTargetDeadband(0.5);
+        shuntCompensator.setVoltageRegulatorOn(true);
+        assertEquals("Shunt compensator 'SHUNT': invalid value (NaN) for voltage setpoint (voltage regulator is on)",
+                assertThrows(ValidationException.class, () -> shuntCompensator.setTargetV(Double.NaN)).getMessage());
+        network.setMinimumAcceptableValidationLevel(ValidationLevel.EQUIPMENT);
+        shuntCompensator.setTargetV(Double.NaN);
+    }
+
+    @Test
+    void updateWithInvalidTargetDeadband() {
+        Network network = FourSubstationsNodeBreakerFactory.create();
+        ShuntCompensator shuntCompensator = network.getShuntCompensator("SHUNT");
+        shuntCompensator.setTargetV(100.0);
+        shuntCompensator.setTargetDeadband(0.5);
+        shuntCompensator.setVoltageRegulatorOn(true);
+        assertEquals("Shunt compensator 'SHUNT': Undefined value for target deadband of regulating shunt compensator",
+                assertThrows(ValidationException.class, () -> shuntCompensator.setTargetDeadband(Double.NaN)).getMessage());
+        network.setMinimumAcceptableValidationLevel(ValidationLevel.EQUIPMENT);
+        shuntCompensator.setTargetDeadband(Double.NaN);
+    }
 }
