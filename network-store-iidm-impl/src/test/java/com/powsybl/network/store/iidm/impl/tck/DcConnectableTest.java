2fa70b7a846d2525d70ba00f8cede681c0b613f9
Joris Mancini <joris.mancini_externe@rte-france.com>
2026-07-06 15:31:27 +0200

    fix: tck overrides because detailed DC model is not implemented
    Add resistance to DC switch (#3863)
    Add active power limits to AC/DC converters (#3940) - beta feature
    (also in v7.2.2) Fix setConnectableBus with variants (#3895)
    DC connectable connect & disconnect method (#3942)
    Merge networks: improve exception message when duplicates are found (#3962)
    
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/tck/DcConnectableTest.java
+++ network-store-iidm-impl/src/test/java/com/powsybl/network/store/iidm/impl/tck/DcConnectableTest.java
@@ -1,22 +1,23 @@
 /**
- * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.powsybl.network.store.iidm.impl.tck;
 
-import com.powsybl.iidm.network.tck.AbstractConnectableTest;
+import com.powsybl.iidm.network.tck.AbstractDcConnectableTest;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
- */
-public class ConnectableTest extends AbstractConnectableTest {
+public class DcConnectableTest extends AbstractDcConnectableTest {
+
+    @Override
+    @Test
+    public void connectAndDisconnect() {
+    }
 
     @Override
     @Test
     public void connectAndDisconnectAcDcConverter() {
-        // FIXME
     }
 }
